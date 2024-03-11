58db8565e6976fc948e340b9e56d45eddbbae6ad
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-11 17:08:42 +0100

    sm8450-common: Inherit aosp dfcm
    Change-Id: I119061ee00c793708f57d8297ecf8b683c22420d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -245,7 +245,7 @@ endif
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/vintf/framework_manifest.xml
 
