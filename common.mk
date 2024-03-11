dba081e177ccb9f4686bbd1e1a3839e5844a1eb6
Bruno Martins <bgcngm@gmail.com>
2024-03-11 17:23:49 +0100

    sm8450-common: Migrate to common libqti-perfd-client and power-libperfmgr
    Change-Id: I22dd0baaf1594db1650bccf37b4d167b50135174

--- common.mk
+++ common.mk
@@ -361,10 +361,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # PowerShare
 PRODUCT_PACKAGES += \
@@ -420,6 +418,10 @@ $(foreach sku, taro diwali cape ukee, \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
 # Telephony
