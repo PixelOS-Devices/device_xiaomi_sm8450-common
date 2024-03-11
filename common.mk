ffa347786bf01885484c74121f1b748fd4af9188
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-11 17:27:24 +0100

    sm8450-common: Import powerhint.json from lahaina
    from https://github.com/PixelOS-Devices/device_oneplus_sm8350-common/blob/fourteen-qpr1/configs/powerhint_lahaina.json
    
    Change-Id: I2ddb226cfd379b6b287a4393058c12dd579d38de

--- common.mk
+++ common.mk
@@ -293,6 +293,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QMI
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti_vendor:64 # Needed by CNE app
