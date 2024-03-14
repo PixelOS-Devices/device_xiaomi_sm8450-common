da715b305e303667eb854baab21aefe9dd3a7cc7
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-14 09:08:32 +0100

    sm8450-common: Add support for Dolby Atmos
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ia97eb58c63d02e95f546fd35874d0201525331c2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -236,6 +236,7 @@ $(foreach nfc_sku, $(call to-upper, $(TARGET_NFC_SUPPORTED_SKUS)), \
 endif
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/vintf/device_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
