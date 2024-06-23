2e908ab0f67085d5694bdad811e12b508bee9640
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    sm8450-common:: Move to new RFS install_symlink targets
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- common.mk
+++ common.mk
@@ -20,6 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sm8450-common/sm8450-common-vendor.mk)
 
