e938bce19e280e21ce58df0bfbf890d3e7a5e3c7
rahul <rahul@aospa.co>
2024-06-26 19:13:53 +0000

    sm8450-common: Build C2 libs

--- common.mk
+++ common.mk
@@ -295,6 +295,9 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     libOmxCore \
+    android.hardware.media.c2@1.0.vendor \
+    android.hardware.media.c2@1.1.vendor \
+    android.hardware.media.c2@1.2.vendor \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
     libstagefrighthw \
