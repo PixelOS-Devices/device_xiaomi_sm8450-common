2a4037a4eaa8eadbe0b92772806418336e7592c6
someone5678 <someone5678@users.noreply.github.com>
2024-07-25 01:09:33 +0900

    sm8450-common: Build android.hardware.audio@7.1-impl
    Change-Id: Ia121731033f717c92d8924b8110d906a68916445

--- common.mk
+++ common.mk
@@ -50,7 +50,7 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.0-impl:64 \
+    android.hardware.audio@7.1-impl:64 \
     android.hardware.audio.effect@7.0-impl:64 \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl:64 \
