98b365e47fa48a23c3ade51f962499c2f1809be8
Vijayanand Jitta <vjitta@qti.qualcomm.com>
2024-09-05 21:51:59 +0100

    sm8450-common: Enable allow_file_spec_access on waipio
    Allow speculative page faults on file backed pages
    by setting allow_file_spec_access on waipio.
    
    Change-Id: I970f2b85d000adb3b779a00833732d73f7932d79
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: rahul <rahul@aospa.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -105,7 +105,9 @@ BOARD_VENDOR_RAMDISK_FRAGMENT.dlkm.KERNEL_MODULE_DIRS := top
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
-    mtdoops.fingerprint=$(LINEAGE_VERSION)
+    mtdoops.fingerprint=$(LINEAGE_VERSION) \
+    allow_file_spec_access
+
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
