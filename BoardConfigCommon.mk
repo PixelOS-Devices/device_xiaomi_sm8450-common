52c0bb630f1cd813b4e19d611c25ed454002a999
Neeraj Upadhyay <neeraju@qti.qualcomm.com>
2024-09-05 21:53:29 +0100

    sm8450-common: Add default affinity bootarg for Waipio
    Add irqaffinity bootarg, to affine irqs to cores 0-3 by default.
    
    Change-Id: I6d7461573c14ae9b5215d132b0e926d55626d13b
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: rahul <rahul@aospa.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -106,7 +106,8 @@ BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
     mtdoops.fingerprint=$(LINEAGE_VERSION) \
-    allow_file_spec_access
+    allow_file_spec_access \
+    irqaffinity=0-3
 
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
