c1c927570f76b9ce6f1c98f945abb99981b14263
Arian <arian.kulmer@web.de>
2024-10-06 18:31:15 +0200

    sm8450-common: Switch to python extract-utils
    Change-Id: I2bc74ab7331516a6e7958fd9238b0e7d2e66872c

---
+++ extract-files.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixup_remove_arch_suffix,
+    lib_fixup_remove_proto_version_suffix,
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_clang_rt_ubsan,
+    libs_proto_3_9_1,
+    libs_proto_21_12,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/sm8450-common',
+    'hardware/qcom-caf/sm8450',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    libs_proto_21_12: lib_fixup_remove_proto_version_suffix,
+    (
+        'vendor.qti.hardware.dpmservice@1.0',
+        'vendor.qti.hardware.dpmservice@1.1',
+        'vendor.qti.hardware.qccsyshal@1.0',
+        'vendor.qti.hardware.qccsyshal@1.1',
+        'vendor.qti.hardware.qccvndhal@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.diaghal@1.0',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'com.qualcomm.qti.dpm.api@1.0',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libagm',
+        'libar-pal',
+        'libpalclient',
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libinput_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed(
+            'android.media.audio.common.types-V2-cpp.so',
+            'android.media.audio.common.types-V3-cpp.so',
+        ),
+    (
+        'vendor/bin/hw/android.hardware.security.keymint-service-qti',
+        'vendor/lib64/libqtikeymint.so',
+    ): blob_fixup()
+        .replace_needed(
+            'android.hardware.security.keymint-V1-ndk_platform.so',
+            'android.hardware.security.keymint-V1-ndk.so',
+        )
+        .replace_needed(
+            'android.hardware.security.secureclock-V1-ndk_platform.so',
+            'android.hardware.security.secureclock-V1-ndk.so',
+        )
+        .replace_needed(
+            'android.hardware.security.sharedsecret-V1-ndk_platform.so',
+            'android.hardware.security.sharedsecret-V1-ndk.so',
+        )
+        .add_needed('android.hardware.security.rkp-V1-ndk.so'),
+    'vendor/bin/qcc-trd': blob_fixup()
+        .replace_needed(
+            'libgrpc++_unsecure.so', 'libgrpc++_unsecure_prebuilt.so'
+        ),
+    'vendor/etc/media_codecs_c2_audio.xml': blob_fixup()
+        .regex_replace('.+media_codecs_dolby_audio.+\n', ''),
+    (
+       'vendor/etc/media_codecs_cape.xml',
+       'vendor/etc/media_codecs_diwali_v0.xml',
+       'vendor/etc/media_codecs_diwali_v1.xml',
+       'vendor/etc/media_codecs_diwali_v2.xml',
+       'vendor/etc/media_codecs_taro.xml',
+       'vendor/etc/media_codecs_ukee.xml',
+    ): blob_fixup()
+        .regex_replace('.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).+\n', ''),
+    'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
+        .add_line_if_missing('gettid: 1'),
+    'vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy': blob_fixup()
+        .add_line_if_missing('setsockopt: 1'),
+    'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
+        .regex_replace('.+dolby.+\n', ''),
+    'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
+        .add_needed('libhidlbase_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sm8450-common',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
