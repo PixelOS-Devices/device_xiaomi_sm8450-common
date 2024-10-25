c474a52936863b2d3aa0b71874e976e38d045079
Arian <arian.kulmer@web.de>
2024-10-25 15:24:02 +0200

    sm8450-common: Allow gettid syscall for wfdhdcphalservice
    10-25 15:22:18.582     1     1 I init    : starting service 'wfdhdcphalservice'...
    10-25 15:22:18.595     1     1 I init    : ... started service 'wfdhdcphalservice' has pid 4702
    10-25 15:22:16.536  4702  4702 E wfdhdcphal: wfdhdcphalservice starting...
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: failed to get path of fd 5: No such file or directory
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: allowing syscall: connect
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: allowing syscall: fcntl
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: allowing syscall: sendto
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: allowing syscall: socket
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: allowing syscall: writev
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: compile_file: <fd>(33): nonexistent syscall 'fstatat'
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: compile_file: <fd>(52): nonexistent syscall 'open'
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: compile_file: <fd>(53): nonexistent syscall 'ugetrlimit'
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: compile_file: <fd>(64): nonexistent syscall 'readlink'
    10-25 15:22:16.536  4702  4702 W wfdhdcphalservice: libminijail[4702]: logging seccomp filter failures
    10-25 15:22:16.538  4702  4702 E wfdhdcphalservice: libminijail[4702]: blocked syscall: gettid
    10-25 15:22:18.622     1     1 I init    : Service 'wfdhdcphalservice' (pid 4702) received signal 31
    10-25 15:22:18.623     1     1 I init    : Sending signal 9 to service 'wfdhdcphalservice' (pid 4702) process group...
    10-25 15:22:18.623     1     1 I libprocessgroup: Removed cgroup /sys/fs/cgroup/uid_1000/pid_4702
    10-25 15:22:18.623     1     1 E init    : process with updatable components 'wfdhdcphalservice' exited 4 times in 4 minutes
    10-25 15:22:18.627     1     1 I init    : processing action (sys.init.updatable_crashing=1) from (/system/etc/init/flags_health_check.rc:10)
    10-25 15:22:18.628     1     1 I init    : starting service 'exec 37 (/system/bin/flags_health_check UPDATABLE_CRASHING)'...
    10-25 15:22:18.637     1     1 I init    : ... started service 'exec 37 (/system/bin/flags_health_check UPDATABLE_CRASHING)' has pid 4703
    10-25 15:22:18.637     1     1 I init    : SVC_EXEC service 'exec 37 (/system/bin/flags_health_check UPDATABLE_CRASHING)' pid 4703 (uid 1000 gid 1000+0 context default) started; waiting...
    
    Change-Id: I9ab6ca254a1e6509cfd9de36765496e875407fb0

--- extract-files.py
+++ extract-files.py
@@ -107,6 +107,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_line_if_missing('gettid: 1'),
     'vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy': blob_fixup()
         .add_line_if_missing('setsockopt: 1'),
+    'vendor/etc/seccomp_policy/wfdhdcphalservice.policy': blob_fixup()
+        .add_line_if_missing('gettid: 1'),
     'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
         .regex_replace('.+dolby.+\n', ''),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
